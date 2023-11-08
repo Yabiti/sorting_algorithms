7566ce5711c3d3869e34e01202387e3b86b21c09
Yabiti <yabetseyohannes@gmail.com>
2023-11-08 14:50:11 +0300

    .

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,11 @@
-#include "sort.h"
+import socket
+import sys
 
-/**
- * bubble_sort - sort array in asscending order
- * @array: array to sort
- * @size: size of array
- **/
+HOST = ""
 
-void bubble_sort(int *array, size_t size)
-{
-	size_t a, b, swap;
+PORT = 9000
 
-	if (array == NULL && size < 2)
-	{
-		return;
-	}
-	a = 0;
-	while (a < size - 1)
-	{
-		b = 0;
-		while (b < size - 1)
-		{
-			if (array[b] > array[b + 1])
-			{
-				swap = array[b];
-				array[b] = array[b + 1];
-				array[b + 1] = swap;
-				print_array(array, size);
-			}
-			b++;
-		}
-		a++;
-	}
-}
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("socket has been created")
+
